2df36b2bb290776589dce38215a89647dfd9502b
Rijwan <rijwan.matin@iproperty.com>
2017-10-04 16:20:41 +0800

    updating routes

--- api/routes/cvRoutes.ts
+++ api/routes/cvRoutes.ts
@@ -1,19 +1,19 @@
 'use strict';
 
 var resume = require('../../cv')
-module.exports = function(app: any) {
+module.exports = function(CvApp: any) {
   var cv = require('../controllers/cvController');
 
-  app.get('/', (req: any, res: any) =>{
+  CvApp.get('/', (req: any, res: any) =>{
     res.json(resume)
   })
 
-  app.route('/cv')
+  CvApp.route('/cv')
     .get(cv.list_all_cvs)
     .post(cv.create_a_cv)
     .delete(cv.delete_all_cv)
 
-    app.route('/cv/:cvId')
+    CvApp.route('/cv/:cvId')
     .get(cv.read_a_cv)
     .put(cv.update_a_cv)
     .delete(cv.delete_a_cv)
