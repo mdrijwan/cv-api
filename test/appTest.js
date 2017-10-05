d27e072bc469a6f4c83b9765da6b3f731dad08c0
Rijwan <rijwan.matin@iproperty.com>
2017-10-05 11:45:32 +0800

    adding mock unit test

--- test/appTest.js
+++ test/appTest.js
@@ -1,11 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const chai_1 = require("chai");
-const cv = require('../app');
 var port = 3000;
-describe('CVapi Port', () => {
+describe('Api Port', () => {
     it('the server should run on the port 3000', () => {
-        // assert.equal.(cv(), 'port = 3000')
-        chai_1.expect(port).to.equal(3000);
+        chai_1.assert.equal(port, 3000);
     });
 });
--- test/appTest.ts
+++ test/appTest.ts
@@ -1,11 +1,9 @@
 import {assert} from 'chai';
 import {expect} from 'chai';
-const cv = require('../app')
 var port = 3000
 
-describe('CVapi Port', () => {
+describe('Api Port', () => {
     it('the server should run on the port 3000', () => {
-        // assert.equal.(cv(), 'port = 3000')
-        expect(port).to.equal(3000)
+        assert.equal(port, 3000)
     });
 });
